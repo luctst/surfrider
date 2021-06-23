368cfd4f3585deb0d00e28a8e66a35952db1d537
luctst <lucas.tostee@gmail.com>
2021-06-23 16:21:36 +0200

    Lol

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app" class="container">
-    <header class="row mt-5 mb-5">
+    <header class="row mt-5 mb-3">
       <div class="col-12">
         <div class="input-group">
           <input
@@ -11,17 +11,11 @@
             aria-describedby="button-addon2"
             v-model="city"
           />
-          <button
-            class="btn btn-outline-secondary"
-            type="button"
-            id="button-addon2"
-            @click="showDate = !showDate"
-          >
-            {{ date }}
-          </button>
-          <vc-calendar v-if="showDate" @dayclick="populateDate"></vc-calendar>
         </div>
-        <div class="suggestions border border-dark  rounded border-top-0" v-if="showSuggestions">
+        <div
+          class="suggestions border border-dark  rounded border-top-0"
+          v-if="showSuggestions"
+          v-click-outside="hide">
           <ul>
             <li
               v-for="(sug, i) in suggestions"
@@ -33,33 +27,110 @@
         </div>
       </div>
     </header>
+    <main class="container">
+      <div class="row" v-if="showLoader">
+        <div class="col-12 d-flex justify-content-center">
+          <Loader />
+        </div>
+      </div>
+      <template v-if="weather">
+        <header class="d-flex">
+          <button
+            v-for="(tmp, i) in weather.ts"
+            :key="i"
+            :class="{ active: i === dateActive }"
+            @click="dateActive = i">
+            {{ parseDate(tmp) }}
+          </button>
+        </header>
+        <section class="row title">
+          <div class="col-12 d-flex align-items-center">
+            <h1 class="fs-2">{{ weather.city }}</h1>
+            <small class="text-info">{{ dateActiveParse }}</small>
+          </div>
+        </section>
+        <section class="row data mt-5">
+          <template v-for="(value, name) in weather">
+            <div
+              v-if="!weatherKeysToExclude.includes(name)"
+              :key="name"
+              class="mb-5 border-warning border-bottom">
+              <h2>{{ name }}</h2>
+            </div>
+          </template>
+          <div></div>
+        </section>
+      </template>
+    </main>
   </div>
 </template>
 
 <script>
+import ClickOutside from 'vue-click-outside';
 import axios from 'axios';
+
+import Loader from './assets/loader.svg';
 
 export default {
   name: 'App',
+  directives: {
+    ClickOutside,
+  },
+  components: {
+    Loader,
+  },
   data() {
     return {
       city: '',
-      date: 'Date',
       showSuggestions: false,
-      showDate: false,
+      dateActive: 0,
+      weatherKeysToExclude: ['units', 'city', 'warning', 'ts'],
+      showLoader: false,
       suggestions: [],
-      dataToPost: {},
+      weather: null,
+      dataToPost: {
+        lat: 0,
+        long: 0,
+      },
     };
   },
-  methods: {
-    populateDate(data) {
-      this.dataToPost.date = data.date;
-      this.showDate = false;
+  computed: {
+    dateActiveParse() {
+      const dateInstancied = new Date(this.weather.ts[this.dateActive]);
+      return `${dateInstancied.getHours()}H:${dateInstancied.getMinutes()}`;
     },
-    populateCity(cityData) {
-      this.suggestions = [];
+  },
+  methods: {
+    parseDate(date) {
+      const dateInstancied = new Date(date);
+
+      return `${dateInstancied.getDate()}/${dateInstancied.getMonth() + 1}/${dateInstancied.getFullYear()} - ${dateInstancied.getHours()}h:${dateInstancied.getMinutes()}mn`;
+    },
+    async populateCity(cityData) {
       this.showSuggestions = false;
+      this.showLoader = true;
+      this.dataToPost.lat = cityData.center[1];
+      this.dataToPost.long = cityData.center[0];
+      this.suggestions = [];
       this.city = cityData.place_name;
+
+      try {
+        const { data } = await axios.post('https://api.windy.com/api/point-forecast/v2', {
+          lat: this.dataToPost.lat,
+          lon: this.dataToPost.long,
+          parameters: ['temp', 'pressure', 'wind'],
+          model: 'gfs',
+          key: process.env.VUE_APP_KEY,
+        });
+        this.weather = data;
+        this.weather.city = this.city;
+        this.showLoader = false;
+      } catch (error) {
+        console.error(error.message);
+      }
+    },
+    hide() {
+      this.showSuggestions = false;
     },
   },
   watch: {
@@ -74,6 +145,13 @@ export default {
         const { data } = await axios.get(
           `https://api.mapbox.com/geocoding/v5/mapbox.places/${this.city}.json?access_token=${process.env.VUE_APP_MAPBOX}`,
         );
+
+        if (this.dataToPost.lat || this.dataToPost.long) {
+          this.dataToPost.lat = 0;
+          this.dataToPost.long = 0;
+          this.suggestions = false;
+          return;
+        }
 
         this.suggestions = data.features;
         this.showSuggestions = true;
@@ -94,6 +172,9 @@ export default {
 
     .suggestions {
       padding: 12px;
+      z-index: 10;
+      position: relative;
+      background-color: #fff;
 
       ul {
         padding-left: 0px;
@@ -115,6 +196,48 @@ export default {
       position: absolute;
       right: 0;
       top: 70px;
+    }
+  }
+
+  main {
+    header {
+      height: 6vh;
+      align-items: flex-start;
+      overflow: auto;
+
+      button {
+        flex: 0 0 auto;
+        font-size: 12px;
+        font-weight: 500;
+        line-height: 18px;
+        border: 1px solid transparent;
+        border-radius: 2em;
+        padding: 0 10px;
+        margin: 0 1.125em .333em 0;
+        line-height: 22px;
+        color: #0366d6;
+        opacity: 0.6;
+        background-color: #f1f8ff;
+      }
+
+      .active {
+        color: #0366d6;
+        opacity: 1;
+      }
+    }
+
+    .title {
+      h1 {
+        margin-right: 8px;
+      }
+    }
+
+    .data {
+      div {
+        h2  {
+          font-size: 4em;
+        }
+      }
     }
   }
 }
